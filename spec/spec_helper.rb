8076b1cb957fb6fe2c0ab71e8eff46774db2bc7b
Kay Robbins <krobbins@cs.utsa.edu>
2011-11-02 19:21:40 -0500

    Added an about page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,12 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However, 
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
   
-end
 
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  
-end
 
 # --- Instructions ---
 # - Sort through your spec_helper file. Place as much environment loading 
@@ -28,10 +27,7 @@ end
 
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -59,4 +55,12 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+  
+  ## Added this Hac
+end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  
 end
